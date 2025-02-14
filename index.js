9ad0c05aea4da83c5ab06b61baf4c5cc2ed0bf3d
Riley Lodge <riley.lodge@gmail.com>
2025-02-14 15:12:49 +1030

    Exercise 3.15-3.18

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const unkownEndpoint = (request, response) => {
-  response.status(404).send({error: 'Unknown endpoint'})
-}
+
 
 app.use(express.json())
 app.use(requestLogger)
@@ -43,16 +41,37 @@ app.get('/api/persons', (request, response) => {
   })
 
 app.get('/api/persons/:id', (request, response)=>{
+
     Person.findById(request.params.id).then(person => {
       response.json(person)
     })
 })
 
-// app.get('/info', (request, response) => {
-//     const date = new Date()
-//     const personsCount = persons.length
-//     response.send(`<p>Phonebook has info for ${personsCount} people</p><p>${date}</p>`)
-//   })
+app.get('/info', (request, response) => {
+    const date = new Date()
+    Person.find({}).then(persons => {
+      const personsCount = persons.length
+      response.send(`<p>Phonebook has info for ${personsCount} people</p><p>${date}</p>`)
+    })
+  })
+
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+    console.log("Updating number for:",person)
+  
+    Person.findOneAndUpdate({name: body.name}, person, { new: true }).then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 
 
 app.post('/api/persons', (request, response) => {
@@ -84,15 +103,30 @@ app.post('/api/persons', (request, response) => {
     
   })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = request.params.id
-//     notes = notes.filter(note => note.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+        
+  })
 
-//     response.status(204).end()
-//   })
+const unkownEndpoint = (request, response) => {
+  response.status(404).send({error: 'Unknown endpoint'})
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 app.use(unkownEndpoint)
-
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
