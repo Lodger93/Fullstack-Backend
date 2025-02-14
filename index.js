836413243231a849e751056773107e2b2318dab7
Riley Lodge <riley.lodge@gmail.com>
2025-02-14 17:08:33 +1030

    Exercise 3.19-3.21

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-
-
 app.use(express.json())
 app.use(requestLogger)
 // console.log("using morgan")
@@ -58,40 +56,23 @@ app.get('/info', (request, response) => {
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    console.log("Updating number for:",person)
-  
-    Person.findOneAndUpdate({name: body.name}, person, { new: true }).then(updatedPerson => {
-      response.json(updatedPerson)
-    })
+    const {name, number} = request.body
+    
+    Person.findOneAndUpdate(
+      {name: name}, 
+      {name, number}, 
+      { new: true, runValidators: true, context: 'query' })
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
     .catch(error => next(error))
 })
 
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     
     const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    // const nameAlreadyUsed = persons.some(person => person.name === body.name)
-    
-    // if(nameAlreadyUsed){
-    //   return response.status(400).json({
-    //   error: 'Name must be unique'
-    //   })
-    // }
-
     const person = new Person({
       name: body.name,
       number: body.number,
@@ -99,7 +80,7 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson=> {
       response.json(savedPerson)
-    })
+    }).catch(error => next(error))
     
   })
 
@@ -107,8 +88,7 @@ app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
       response.status(204).end()
     })
-    .catch(error => next(error))
-        
+    .catch(error => next(error)) 
   })
 
 const unkownEndpoint = (request, response) => {
@@ -120,7 +100,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
