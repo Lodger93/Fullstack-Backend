67fa50ef7f762234e89ff027cd6f413f4bb98ee0
Riley Lodge <riley.lodge@gmail.com>
2025-02-21 11:02:28 +1030

    Updated structure

--- models/note.js
+++ models/note.js
@@ -1,25 +1,10 @@
-
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
-
-console.log("Connecting to", url)
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -31,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
